72245d5a4dc763d08af327f3673d66e6bd086520
Yoichi-dev <yoichi.developer@gmail.com>
2021-09-05 17:24:59 +0900

    [Bugfix] 自動同行後の空白除去

--- pages/index.vue
+++ pages/index.vue
@@ -142,7 +142,7 @@ export default {
       };
     },
     getComment(commentObj) {
-      if (commentObj.cm != "" || commentObj.cm != null) {
+      if (commentObj.cm != "" || commentObj.cm != undefined) {
         this.commentData.unshift({
           id: commentObj.u,
           name: commentObj.ac,
