4995c20e3f4538dcfe854e1826401bc34c7636be
Yoichi-dev <yoichi.developer@gmail.com>
2021-09-08 12:08:18 +0900

    [Add] 塔追加

--- pages/index.vue
+++ pages/index.vue
@@ -55,8 +55,6 @@ export default {
           }
         });
 
-      console.log(flg);
-      console.log(preFlg);
       if (preFlg) {
         await axios
           .get(`${process.env.API_URL}/api/users/onlive/${this.roomId}`)
@@ -187,11 +185,11 @@ export default {
         // ギフト画像の要素を作成
         let giftImgElement = document.createElement("img");
         // 画像を設定
-        if (i <= 5) {
+        if (i <= 6) {
           // giftImgElement.src = `~/assets/image/${i}.png`;
           giftImgElement.src = require(`@/assets/image/${i}.png`);
           giftImgElement.style.width = "50px";
-        } else if (i == 6) {
+        } else if (i == 7) {
           giftImgElement.src = `https://image.showroom-cdn.com/showroom-prod/image/avatar/${gift.av}.png?v=85`;
           giftImgElement.style.width = "50px";
         } else {
